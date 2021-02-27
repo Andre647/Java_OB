6cc8783f268ad7396a61851198bb5371acbb930d
Deca <41493871+Pitucho1@users.noreply.github.com>
2021-02-27 14:20:52 -0300

    programando por linha de comando

---
+++ Aula03/prog1/OlaMundo.java
@@ -0,0 +1,8 @@
+class OlaMundo{
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo!"); //comentario de uma linha
+        System.out.println("Socorro");
+        System.out.println("Rapazzzz");
+    }
+
+}
