8437e51188c4da2f94bfb758aba136d06093fcdd
Deca <41493871+Pitucho1@users.noreply.github.com>
2021-03-15 10:16:05 -0300

    menu executando

--- Aula05/src/monteiro/andre/Sistema.java
+++ Aula05/src/monteiro/andre/Sistema.java
@@ -1,7 +1,29 @@
 package monteiro.andre;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private boolean executarSistema;
+    private Scanner scanner;
+
     public void executar(){
-        System.out.println("Aqui");
+        int opcao;
+        while(executarSistema){
+            exibirMenu();
+            opcao = scanner.nextInt();
+        }
+    }
+    public Sistema(){
+        this.executarSistema = true;
+        this.scanner = new Scanner(System.in);
+    }
+    private void exibirMenu(){
+        System.out.println("Bem vindo ao Mauá Bank");
+        System.out.println("1 - Visualizar Saldo");
+        System.out.println("2 - Depositar Dinheiro");
+        System.out.println("3 - Sacar Dinheiro");
+        System.out.println("4 - Transferir Dinheiro");
+        System.out.println("5 - Pagar Conta");
+        System.out.println("0 - Encerrar Sistema");
     }
 }
