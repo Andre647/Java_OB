a9a042abf7cab58500f3daf33acef8fdd12fe250
Deca <41493871+Pitucho1@users.noreply.github.com>
2021-03-18 11:21:58 -0300

    ValidandoCPF

---
+++ Aula05-teoria/src/monteiro/andre/Main.java
@@ -0,0 +1,9 @@
+package monteiro.andre;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String cpf = "502.482.988-36";
+        System.out.println("Cpf valido:" + ValidadorCPF.validarCpf(cpf));
+    }
+}
