df162296b26e69d9da65f9fa90768e23d6b17b30
Samuel Antunes <antunessamuel0@gmail.com>
2021-04-02 15:42:45 -0300

    Comecei a classe transacoes e fiz algumas mudancas na class Contas e Cliente

--- Atividade1/src/monteiro/andre/Transacoes.java
+++ Atividade1/src/monteiro/andre/Transacoes.java
@@ -3,9 +3,34 @@ package monteiro.andre;
 import java.util.Random;
 
 public class Transacoes {
-    //private static int getRandomNumberInRange(int min, int max) {
-        //Random r = new Random();
-        //return r.nextInt((max - min) + 1) + min;
+    private static int getRandomNumberInRange(int min, int max) {
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+    public String geraQRCode(Contas destino, double valor){
+        return destino.idConta + ";" + destino.cliente.getNome() + ";" + valor + ";" + getRandomNumberInRange(1000, 9999); 
+    }
 
-    }//Gera codigo de cada operacao
-    //private boolean executarTransacao();
+    public boolean stringValida(String QRCode){
+        String[] dados = QRCode.split(";");
+        int idContaDestino = Integer.parseInt(dados[0]);
+        String nomeDestino = dados[1];
+        int valor = Double.parseDouble(dados[2]);
+        int numAleatorio = Integer.parseInt(dados[3]);
+        //precisa de algum jeito de pegar a conta e o id
+        return true;
+    }
+
+    public boolean transacaoValida(Contas pagante, Contas destino, String QRCode) {
+
+        //retorna se a transacao eh valida
+        //metodo stringValida eh chamado aqui
+        return true;
+    }
+
+    public void executarTransacao() {
+        //executa a transacao
+    }
+
+}//Gera codigo de cada operacao
+//private boolean executarTransacao();
